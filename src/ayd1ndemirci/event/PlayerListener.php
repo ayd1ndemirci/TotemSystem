097168149b06aa46fbd34cb1f41b9fd5eebf6346
TrFolwe <monitorminor28@gmail.com>
2023-06-04 21:08:43 +0300

    A database problem is fixed

--- src/ayd1ndemirci/event/PlayerListener.php
+++ src/ayd1ndemirci/event/PlayerListener.php
@@ -3,19 +3,39 @@
 namespace ayd1ndemirci\event;
 
 use ayd1ndemirci\Main;
+use ayd1ndemirci\provider\SQLiteDatabase;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\Server;
+use SOFe\AwaitGenerator\Await;
 
 class PlayerListener implements Listener
 {
-    function onJoin(PlayerJoinEvent $event):void
+
+    /*** @var SQLiteDatabase */
+    private SQLiteDatabase $database;
+
+    public function __construct() {
+        $this->database = Main::getInstance()->getDatabase();
+    }
+
+    /**
+     * @param PlayerJoinEvent $event
+     * @return void
+     */
+    public function onPlayerJoin(PlayerJoinEvent $event):void
     {
         $player = $event->getPlayer();
-        Main::getInstance()->getDatabase()->getPlayerToken($player->getName(), function ($token) use ($player) {
-            if (empty($token)) {
-                Main::getInstance()->getDatabase()->addPlayer($player->getName());
-            }
+        Await::f2c(function () use($player) {
+            $rows = (array) yield from $this->database->getPlayerToken($player->getName());
+            if(empty($rows)) $this->database->addPlayer($player->getName());
         });
+    }
+    /**
+     * @param PlayerDeathEvent $event
+     * @return void
+     */
+    public function onPlayerDeath(PlayerDeathEvent $event) :void {
+        $event->setKeepInventory(true);
     }
 }
